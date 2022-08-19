79e68b1d40af50218b9874e21346cc4e74151e70
Alone <34532101+alonelove7@users.noreply.github.com>
2022-08-20 01:41:28 +0430

    main

---
+++ config/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    API_ID = int( os.getenv("api_id","12158050") )
+    API_HASH = os.getenv("api_hash","e7e2e7f23c51a465f62067d8644f34ae")
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001526486348") )
+    CHANNEL_USERNAME = os.getenv("channel_username","King_network7")
+    TOKEN = os.getenv("token","2045343811:AAFK6hL7f1YmjXNcQJSFxZmOmnVYPgwuHaI")
+    DOMAIN  = os.getenv("domain","https://d4.kimo.vip")
+    StringSession = "1AZWarzoBu6xta2bpPgvse6TQRwp3a5zXEOX05DWOrgeOKO9Tlz8XmxkD7nwX0Ng12Btz471BySc78TGvx2OdShCJfG_Chw7NqVqkVqPF8k2TpPOuQkCQZzqpmsZ3Rj2BCbdeZjYuVRm-xmQAQjyAGCWAQU4HLfE9iLLsqPRQJW7ZX67BXvefaR9ZjolCZ1pAsVN96eNnHyOn1Wh3984u2Zu22j9mLS6nQWawixBfsQphjtu5cCNqyei25oFUF_mZcFm0xrxwJYDRBNoPr3wZNMzwF9_keOMK4tFocBe8vMWbBIfz5uh_nG4q9G23WJn5HjLJWmGkP6XGxJdE0n7c91tb7L0hE0I="
