d2b38b86c3e8a752bc0806b55fa2f19c646975df
Alone <34532101+alonelove7@users.noreply.github.com>
2022-09-16 12:47:02 +0430

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -7,4 +7,3 @@ class Config:
     CHANNEL_USERNAME = os.getenv("channel_username","King_network7")
     TOKEN = os.getenv("token","2045343811:AAFK6hL7f1YmjXNcQJSFxZmOmnVYPgwuHaI")
     DOMAIN  = os.getenv("domain","https://d4.kimo.vip")
-    StringSession = "1AZWarzoBu6xta2bpPgvse6TQRwp3a5zXEOX05DWOrgeOKO9Tlz8XmxkD7nwX0Ng12Btz471BySc78TGvx2OdShCJfG_Chw7NqVqkVqPF8k2TpPOuQkCQZzqpmsZ3Rj2BCbdeZjYuVRm-xmQAQjyAGCWAQU4HLfE9iLLsqPRQJW7ZX67BXvefaR9ZjolCZ1pAsVN96eNnHyOn1Wh3984u2Zu22j9mLS6nQWawixBfsQphjtu5cCNqyei25oFUF_mZcFm0xrxwJYDRBNoPr3wZNMzwF9_keOMK4tFocBe8vMWbBIfz5uh_nG4q9G23WJn5HjLJWmGkP6XGxJdE0n7c91tb7L0hE0I="
